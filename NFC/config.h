a491c90e03c4c6d8fd72de6a7bb35ed848942a82
Thaingern <130678128+thaingern001@users.noreply.github.com>
2024-03-04 16:17:53 +0700

    Add files via upload

---
+++ NFC/config.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID    "KUWIN-IOT"
+#define WIFI_PASS    ""
+#define MQTT_BROKER  "iot.cpe.ku.ac.th"
+#define MQTT_USER    "b6610502030"  // ไม่ต้องใส่เครื่องหมาย <>
+#define MQTT_PASS    "thaingern.p@ku.th"      // ไม่ต้องใส่เครื่องหมาย <>
+#define TOPIC_PREFIX "b6610502030"  // ไม่ต้องใส่เครื่องหมาย <>
