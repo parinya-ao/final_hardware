26319c5022e55481f8e476f3111878b01ec757cf
noniie <140971776+noniie@users.noreply.github.com>
2024-03-05 15:02:31 +0700

    fix detection

--- detection/main.py
+++ detection/main.py
@@ -1,13 +1,12 @@
 import requests
-import cv2
-from time import sleep 
+from time import sleep
+import face_recognition
 
 # from umqtt.simple import MQTTClient
-
 imagePath = r'img3.jpg'
-cascPath = "haarcascade_frontalface_default.xml"
 
-token = ''
+
+token = 'z6kZjpcYQj3xiwnPU5hr4DFBvVmY3tPAFloQieY3DUT'
 payload = {'message' : 'found someone'
           ,'notificationDisabled' : False}
 
@@ -15,12 +14,11 @@ while True :
     img_data = requests.get("http://10.2.6.215/capture").content
     with open('img3.jpg', 'wb') as handler:
         handler.write(img_data)
-    image = cv2.imread(imagePath)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faceCascade = cv2.CascadeClassifier(cascPath)
-    faces = faceCascade.detectMultiScale(gray)
-    print(f'There are {len(faces)} faces found.')
-    if len(faces)>=1:
+
+    image = face_recognition.load_image_file('img3.jpg')
+    face_location = face_recognition.face_locations(image)
+    print(f'There are {len(face_location)} faces found.')
+    if len(face_location)>=1:
         r = requests.post('https://notify-api.line.me/api/notify'
                 , headers={'Authorization' : 'Bearer {}'.format(token)}
                 , params = payload
